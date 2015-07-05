5fe4db6ff1a21d99e96540638cddda2f83e678ab
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-07-05 22:30:28 +0900

    highlight

--- Gemfile
+++ Gemfile
@@ -6,8 +6,10 @@ gem 'middleman-deploy'
 gem 'middleman-google-analytics'
 gem 'middleman-livereload'
 gem 'middleman-slim'
+gem 'middleman-syntax'
 
 gem 'materialize-sass'
+gem 'redcarpet'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-jquery'
--- config.rb
+++ config.rb
@@ -53,6 +53,10 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true
+
+activate :syntax
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
